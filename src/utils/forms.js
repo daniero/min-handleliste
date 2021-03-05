22e62163e9a66ceecb1ac24d1eb7551257837b4e
Daniel Rødskog <danielmero@gmail.com>
2021-03-05 23:22:11 +0100

    Konverter getFormData til Typescript

--- src/utils/forms.js
+++
@@ -1,8 +0,0 @@
-export function getFormData(formEvent) {
-  const formData = new FormData(formEvent.target);
-  const data = {};
-  for (const [key, value] of formData.entries()) {
-    data[key] = value;
-  }
-  return data;
-}
