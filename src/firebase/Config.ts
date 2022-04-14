eee70733dff0dfb6058722f308812460d0e7398d
Daniel Rødskog <daniel.rodskog@norgesgruppen.no>
2022-04-14 20:18:17 +0200

    Refaktorer Firebase-config

--- src/firebase/Config.ts
+++ src/firebase/Config.ts
@@ -1,10 +1,8 @@
-export interface FirebaseConfig {
-  appId: string;
-  apiKey: string;
-  projectId: string;
-  databaseURL: string;
-  authDomain?: string;
-  storageBucket?: string;
-  measurementId?: string;
-  messagingSenderId?: string;
-}
+import type { FirebaseOptions } from "firebase/app"
+
+export const config: FirebaseOptions = {
+  appId: process.env.REACT_APP_APP_ID!,
+  apiKey: process.env.REACT_APP_API_KEY!,
+  databaseURL: process.env.REACT_APP_DATABASE_URL!,
+  projectId: process.env.REACT_APP_PROJECT_ID!,
+};
--- src/firebase/setup.ts
+++ src/firebase/setup.ts
@@ -1,16 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { FirebaseConfig } from "./Config";
-
-const config: FirebaseConfig = {
-  appId: process.env.REACT_APP_APP_ID!,
-  apiKey: process.env.REACT_APP_API_KEY!,
-  databaseURL: process.env.REACT_APP_DATABASE_URL!,
-  projectId: process.env.REACT_APP_PROJECT_ID!,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN!,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET!,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID!,
-  // measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
+import { config } from "./Config";
 
 export const setup = () => {
   return initializeApp(config);
