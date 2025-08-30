f5bc79c0a2f9daa140265d9a3ab113f570b9f5fb
Daniel Rødskog <danielmero@gmail.com>
2025-08-31 01:28:01 +0200

    Slett diverse og oppdater alt mulig

--- src/index.tsx
+++ src/index.tsx
@@ -21,8 +21,3 @@ root.render(
     </Wiring>
   </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
